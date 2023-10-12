d6ee45bebe71852307cdcb8856f7ce1ff8b49820
Francesco Paolo Sferratore <sferratore.francescopaolo00@gmail.com>
2023-10-12 16:20:48 +0200

    Added CalculatorWebApp exercise

---
+++ CalculatorWebApp/calculatorwebapp/src/main/java/CalculatorController.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+
+//RequestInfoController uses the "request" object set by the environment to show information about the request received from the server to the user.
+@WebServlet("/Calculate")
+public class CalculatorController extends HttpServlet {
+    
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        int num1 = Integer.parseInt(request.getParameter("number1"));
+        int num2 = Integer.parseInt(request.getParameter("number2"));
+        String operation = request.getParameter("operation");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        if(operation.equals("sum")){
+            out.println("<p>" + (num1 + num2) + "</p>");
+        }
+        else if(operation.equals("subt")){
+            out.println("<p>" + (num1 - num2) + "</p>");
+        }
+        else if(operation.equals("mult")){
+            out.println("<p>" + (num1*num2) + "</p>");
+        }
+        else if(operation.equals("split")){
+            out.println("<p>" + (num1/num2) + "</p>");
+        }
+        else{
+            out.println("<p> WRONG DATA >:(</p>");
+        }
+    }
+
+}
