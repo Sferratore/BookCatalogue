399acf81ad0bccd850724d441937d9da521daecc
Francesco Paolo Sferratore <sferratore.francescopaolo00@gmail.com>
2023-10-12 12:18:41 +0200

    Commented MyFirstJakartaWebApp

--- MyFirstJakartaWebApp/demo/src/main/java/HelloWorld.java
+++ MyFirstJakartaWebApp/demo/src/main/java/HelloWorld.java
@@ -1,14 +1,21 @@
+//"HelloWorld" servlet. (Folder "Java" contains servlets)
+
+//Import of standard libraries and jakarta (Jakarta, as per Maven's rules, needs to be defined in pom.xml before using.)
 import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 
+//Java annotation to link this servlet to the /HelloWorld call ([fullprojecturl]/HelloWorld to call the servlet).
 @WebServlet("/HelloWorld")
+//Servlet class 
 public class HelloWorld extends HttpServlet {
+    //Method activated when the servlet is called with a GET
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws IOException, ServletException
+    throws IOException, ServletException 
     {
-        response.setContentType("text/html");
+        //Defining the html document in response
+        response.setContentType("text/html"); 
         PrintWriter out = response.getWriter();
         out.println("<html>");
         out.println("<head>");
--- MyFirstJakartaWebApp/demo/src/main/webapp/index.jsp
+++ MyFirstJakartaWebApp/demo/src/main/webapp/index.jsp
@@ -1,3 +1,4 @@
+<!--Index page of the project. The first to be sent when user connects to endpoint.-->
 <html>
 <body>
 <h2>Try calling /HelloWorld :-)</h2>
