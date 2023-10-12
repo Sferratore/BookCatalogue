5d8c4a455c70f3d4575f942b8541b2d48eb3f2b2
Francesco Paolo Sferratore <sferratore.francescopaolo00@gmail.com>
2023-10-12 14:53:35 +0200

    Added RequestInfo exercise.

---
+++ RequestInfo/requestinfoexercise/src/main/java/RequestInfoController.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+
+//RequestInfoController uses the "request" object set by the environment to show information about the request received from the server to the user.
+@WebServlet("/RequestInfo")
+public class RequestInfoController extends HttpServlet {
+    
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<head>");
+        out.println("<title>Request Information Example</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h3 style=\"color:green\">Request Information Example</h3>");
+        out.println("Method: " + request.getMethod());
+        out.println("Request URI: " + request.getRequestURI());
+        out.println("Protocol: " + request.getProtocol());
+        out.println("PathInfo: " + request.getPathInfo());
+        out.println("Remote Address: " + request.getRemoteAddr());
+        out.println("Is it secure?: " + request.isSecure());
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    /**
+     * We are going to perform the same operations for POST requests
+     * as for GET methods, so this method just sends the request to
+     * the doGet method.
+     */
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        doGet(request, response);
+    }
+}
---
+++ RequestInfo/requestinfoexercise/target/requestinfoexercise/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Try calling /RequestInfo :-)</h2>
+</body>
+</html>
