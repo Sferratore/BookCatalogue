5c38e537eb63e11d3becd3c5b02b1abe354a131f
Francesco Paolo Sferratore <sferratore.francescopaolo00@gmail.com>
2023-10-16 14:58:07 +0200

    Added new code on DbHandler project.

--- JDBC/DBHandlerProject/src/DbHandler.java
+++ JDBC/DBHandlerProject/src/DbHandler.java
@@ -4,22 +4,43 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import com.mysql.cj.jdbc.result.ResultSetMetaData;
 
+/**
+ * Represents a database handler object.
+ * Its goal is to be an interface to the database.
+ */
 public class DbHandler {
 	
-	//Constructors
-	public DbHandler() {
-		
-	}
+	//----------CONSTRUCTORS------------
+	/**
+	 * Empty, default constructor
+	 */
+	public DbHandler() {}
 	
+	/**
+	 * Constructor with connection variables defined. Creates the connection on instance.
+	 * @param username The username used to log into the database system.
+	 * @param password The password used to log into the database system.
+	 * @param username The username used to log into the database system.
+	 */
 	public DbHandler(String username, String password, String dbname) {   //Constructor that initializes connection
 		connectToDb(username, password, dbname);
 	}
 	
 	
-	//Methods
+	//---------------METHODS----------------
+	/**
+	 * Creates connection to use for the database object.
+	 * @param username The username used to log into the database system.
+	 * @param password The password used to log into the database system.
+	 * @param username The username used to log into the database system.
+	 * 
+	 * @throws ClassNotFoundException in case the static field JConnectionClass contains a wrong connector class.
+	 * @throws SQLException in case an error with operation on DB happens.
+	 * 
+	 * @return Returns a string that describes the result of the operation.
+	 */
 	public String connectToDb(String username, String password, String dbname){
 		try {
 			
@@ -45,6 +66,13 @@ public class DbHandler {
 		
 	}
 	
+	/**
+	 * Queries the db with a query defined by the user.
+	 * @param query contains the query which is user-defined.
+	 * @throws SQLException in case an error with operation on DB happens.
+	 * 
+	 * @return Returns a ResultSet object with the result.
+	 */
 	public ResultSet queryDb(String query) {
 		PreparedStatement prpSt;
 		try {
@@ -57,7 +85,11 @@ public class DbHandler {
 		}			
 	}
 	
-	
+	/**
+	 * Prints all the records into a ResultSet object.
+	 * @param rs the ResultSet object with the values to print.
+	 * @throws SQLException in case an error with operation on DB happens.
+	 */
 	public void printResultSet(ResultSet rs) {
 		
 		String recordString = "";
@@ -80,13 +112,44 @@ public class DbHandler {
 		}
 	}
 	
+	/**
+	 * Queries the DB and prints the result.
+	 * @param query is the query defined by the user.
+	 */
 	public void queryDbAndPrintRs(String query) {
 		printResultSet(queryDb(query));
 	}
 	
-	//Fields
-	 Connection connection;
+	/**
+	 * Closes current DB connection.
+	 * @throws SQLException in case an error with operation on DB happens.
+	 */
+	public void closeConnection() {
+		try {
+			this.connection.close();
+			this.connection = null;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * @return true if DbHandler object has an open connection, false otherwise.
+	 */
+	public boolean isConnectionOpen() {
+		if(this.connection == null)
+			return false;
+		return true;
+	}
+	
+	
+	
+	//--------------------FIELDS-------------------
+	/** Field containing the connection object to the database. */
+	 private Connection connection;
 	 
-	 //Finals
-	 static String JConnectionClass = "com.mysql.cj.jdbc.Driver";
+	 //STATIC FIELDS
+	 /** Static field defining the connection driver class used in the program. */
+	 public static String JConnectionClass = "com.mysql.cj.jdbc.Driver";
 }
