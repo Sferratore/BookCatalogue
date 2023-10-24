83a21f25a388765da7e10f8273225327db9ff038
Sferratore <sferratore.francescopaolo00@gmail.com>
2023-10-24 22:47:34 +0200

    Added exercise ActivityCRUD, need to complete it

---
+++ Spring/ActivityCRUD/src/test/java/com/ActCrud/ActivityCRUD/ActivityCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ActCrud.ActivityCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
